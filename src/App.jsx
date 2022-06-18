b6ac04bea08d09c6e47a58899c418fe906fbd4db
rajesh.kumar@goavega.com-at-012114828737 <rajesh.kumar@goavega.com>
2022-06-18 18:52:27 +0530

    folders added for every-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import About from './components/about/About'
+import Contract from './components/contact/Contract'
+import Experience from './components/experience/Experience'
+import Footer from './components/footer/Footer'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import Portfolio from './components/portfolio/Portfolio'
+import Services from './components/services/Services'
+import Testimonial from './components/testimonials/Testimonial'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonial />
+      <Contract />
+      <Footer />
+    </>
   )
 }
 
